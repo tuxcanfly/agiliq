890a6b668ffba350e187bab514a5a32d9fd59f66
Javed Khan <javed@agiliq.com>
2013-08-21 18:17:55 +0530

    using transaction

--- agiliqcom/myapp/management/commands/sync_myapp.py
+++ agiliqcom/myapp/management/commands/sync_myapp.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+
+from django.db import transaction
+
 from myapp.models import MyModel
 
-from threading import Lock
-
-MYMODEL_LOCK = Lock()
 
 class Command(BaseCommand):
     args = '<my_id my_id ...>'
@@ -11,16 +11,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for my_id in args:
-            created = False
-            try:
-                instance = MyModel.objects.get(my_id=my_id)
-            except MyModel.DoesNotExist:
-                with MYMODEL_LOCK:
-                    try:
-                        instance = MyModel.objects.create(my_id=my_id)
-                        created = True
-                    except MyModel.DoesNotExist:
-                        instance = MyModel.objects.get(my_id=my_id)
+            instance, created = self.get_or_create(my_id)
             self.stdout.write('%s %s' % (instance.id, created))
             instance.delete()
             MyModel.objects.all()
+
+    def get_or_create(self, my_id):
+        created = False
+        try:
+            instance = MyModel.objects.get(my_id=my_id)
+        except MyModel.DoesNotExist:
+            with transaction.commit_on_success():
+                try:
+                    instance = MyModel.objects.create(my_id=my_id)
+                    created = True
+                except MyModel.DoesNotExist:
+                    instance = MyModel.objects.get(my_id=my_id)
+        return instance, created
+
