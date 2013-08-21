4c6cb7814883f3ccdfb5f2ab424ed870707ee633
Javed Khan <javed@agiliq.com>
2013-08-21 17:48:35 +0530

    using locks

--- agiliqcom/myapp/management/commands/sync_myapp.py
+++ agiliqcom/myapp/management/commands/sync_myapp.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from myapp.models import MyModel
 
+from threading import Lock
+
+MYMODEL_LOCK = Lock()
+
 class Command(BaseCommand):
     args = '<my_id my_id ...>'
     help = 'Get or create instace of mymodel with my_id'
@@ -11,8 +15,12 @@ class Command(BaseCommand):
             try:
                 instance = MyModel.objects.get(my_id=my_id)
             except MyModel.DoesNotExist:
-                instance = MyModel.objects.create(my_id=my_id)
-                created = True
+                with MYMODEL_LOCK:
+                    try:
+                        instance = MyModel.objects.create(my_id=my_id)
+                        created = True
+                    except MyModel.DoesNotExist:
+                        instance = MyModel.objects.get(my_id=my_id)
             self.stdout.write('%s %s' % (instance.id, created))
             instance.delete()
             MyModel.objects.all()
